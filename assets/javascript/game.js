85acdca2f16314d9f7e944ea4fd1e21d383ca26e
cplank <cplankshepard@gmail.com>
2019-02-28 15:00:55 -0800

    fixed some bugs added styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,17 @@
-const wordBank = ['umbrella', 'academy', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'handler', 'agents', 'time', 'space', 'strength', 'bend', 'metal', 'rumor', 'hypnosis', 'dead', 'commune',
-    'orphan', 'reginold', 'luther', 'diego', 'allison', 'klaus', 'boy', 'ben', 'vanya', 'chacha', 'hazel', 'agnes', 'doughnuts', 'pogo', 'mom', 'temporal', 'assassins', 'apocalypse', 'violin',
-    'tentacles', 'transformation', 'ghosts', 'murder', 'gorilla', 'chimpanzee', 'robot', 'monocle', 'superheroes', 'villian', 'commission', 'contract', 'mannequin', 'dolores', 'police', 'officer', 'patch',
-    'hargreeves', 'vietnam', 'love', 'october', 'moon', 'astronaut', 'knife', 'vigilante', 'addict', 'traveler', 'extraordinary', 'siblings']
+const wordBank = ['umbrella']
+
+//'academy', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'handler', 'agents', 'time', 'space', 'strength', 'bend', 'metal', 'rumor', 'hypnosis', 'dead', 'commune',
+//'orphan', 'reginold', 'luther', 'diego', 'allison', 'klaus', 'boy', 'ben', 'vanya', 'chacha', 'hazel', 'agnes', 'doughnuts', 'pogo', 'mom', 'temporal', 'assassins', 'apocalypse', 'violin',
+//'tentacles', 'transformation', 'ghosts', 'murder', 'gorilla', 'chimpanzee', 'robot', 'monocle', 'superheroes', 'villian', 'commission', 'contract', 'mannequin', 'dolores', 'police', 'officer', 'patch',
+// 'hargreeves', 'vietnam', 'love', 'october', 'moon', 'astronaut', 'knife', 'vigilante', 'addict', 'traveler', 'extraordinary', 'siblings', 'family', 'paternity']
+
+//Let's make some variables
+let gameBoard = [];
+let guessCounter = 0;
+let guessBox = [];
+let userGuess;
+let winBox;
+let userScore;
 
 //Grab random word from word bank for player to guess
 function generateWord() {
@@ -11,67 +21,71 @@ function generateWord() {
 
 let word = generateWord();
 
-function getGuess(event) {
-    //onkey save letter to letter
-    let code = event.keyCode
-    if (code <= 90 && code >= 65) {
-        let letter = event.key
-        isGuessValid(letter)
-        console.log(letter);
-    } else {
-        alert("Please enter a letter a-z")
-    }
-
-};
-
-function isGuessValid(userGuess) {
-    let x = word.indexOf(userGuess)
-
-    if (x < 0) {
-        //store userGuess in guessBox
-        guessBox.push(userGuess)
-
-    } else { //need to modify gameBoard at x
-        gameBoard[x] = userGuess
-
-    }
-}
-//Once every letter in word has been guessed celebrate, then resetGame
-function wordComplete() {
-    if (gameBoard === word) {
-        celebrate();
-        winBox += 1;
-        resetGame();
-    } else {
-        // dont do nothin
-    }
-}
-
-//I'm declaring these here but know they move to the top
-let gameBoard = "";
-let guessCounter = 0;
-let guessBox = [];
-
-
+//Start game generates a word and then is never start game again
 function startGame() {
-    //generate word
-    //never be start game again
     let y = document.getElementById("game")
     y.onkeyup = getGuess;
     console.log("Game started")
 
+    // Take user input onkey and turn it into a guess
+    function getGuess(event) {
+        //onkey save letter to letter
+        let code = event.keyCode
+        if (code <= 90 && code >= 65) {
+            let letter = event.key
+            isGuessValid(letter)
+            console.log(letter);
+        } else {
+            alert("Please enter a letter a-z")
+        }
+    };
+
+    //Determine if guess matches a letter in the word
+    function isGuessValid(userGuess) {
+        let x = word.indexOf(userGuess)
+
+        if (x < 0) {
+            //store userGuess in guessBox
+            guessBox.push(userGuess)
+            document.getElementById("guesses").textContent = guessBox
+            guessCounter++;
+            document.getElementById("guess-counter").textContent = guessCounter
+            outOfGuesses();
+
+
+        } else { //need to modify gameBoard at x
+            gameBoard[x] = userGuess
+            console.log("You guessed correctly: " + userGuess)
+            console.log(gameBoard)
+            document.getElementById("game-board").textContent = gameBoard
+            wordComplete();
+        }
+    }
+    //Once every letter in word has been guessed celebrate, then resetGame
+    function wordComplete() {
+        if (gameBoard === word) {
+            celebrate();
+            winBox += 1;
+            resetGame();
+        } else {
+            // dont do nothin
+        }
+    }
 }
+
+//Determining if the user as used more than 10 guesses
 function outOfGuesses() {
     // if the user ran out of guesses and didn't win, they lose
-    if (guessCounter === 10) {
+    if (guessCounter === 9) {
         lose();
         // show the word to rub it in their face
         gameBoard = word;
+        resetGame();
     } else {
-
     }
-
 }
+
+//Start the game over
 function resetGame() {
     // then run generate word
     // clear gameBoard, guessBox, and reset guessCounter
@@ -80,20 +94,19 @@ function resetGame() {
     word = generateWord()
     for (let i = 0; i < word.length; i++) {
         //Dashes at every index of gameBoard
-        gameboard[i] = "-"
+        //gameBoard[i] = "-"
     }
 };
 
+//User wasn't able to guess the word
 function lose() {
     alert('you are out of guesses')
 };
+
+//User guessed the word
 function celebrate() {
     alert("Yay! You won!")
 };
 
-let userGuess;
 
-let winBox;
-
-let userScore;
 
