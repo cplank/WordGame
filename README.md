90bf699ff14944ecd85a7b150cf17e8fb824510f
cplank <cplankshepard@gmail.com>
2019-03-01 18:52:42 -0800

    almost working

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# WordGame
-Hangman type word game
+# Umbrella Academy Word Game 
+This is a hangman type word game made using vanilla javaScript. The words used are all associated 
+with the Netflix series <i>The Umbrella Academy</i> and the pictures are from <i>The Umbrella Academy</i>
+comic books. 
+
+# Description 
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,7 @@
-const wordBank = ['umbrella']
-
-//'academy', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'handler', 'agents', 'time', 'space', 'strength', 'bend', 'metal', 'rumor', 'hypnosis', 'dead', 'commune',
-//'orphan', 'reginold', 'luther', 'diego', 'allison', 'klaus', 'boy', 'ben', 'vanya', 'chacha', 'hazel', 'agnes', 'doughnuts', 'pogo', 'mom', 'temporal', 'assassins', 'apocalypse', 'violin',
-//'tentacles', 'transformation', 'ghosts', 'murder', 'gorilla', 'chimpanzee', 'robot', 'monocle', 'superheroes', 'villian', 'commission', 'contract', 'mannequin', 'dolores', 'police', 'officer', 'patch',
-// 'hargreeves', 'vietnam', 'love', 'october', 'moon', 'astronaut', 'knife', 'vigilante', 'addict', 'traveler', 'extraordinary', 'siblings', 'family', 'paternity']
+const wordBank = ['love', 'umbrella', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'handler', 'agents', 'time', 'space', 'strength', 'bend', 'metal', 'rumor', 'hypnosis', 'dead', 'commune',
+    'orphan', 'reginold', 'luther', 'diego', 'allison', 'klaus', 'boy', 'ben', 'vanya', 'chacha', 'hazel', 'agnes', 'doughnuts', 'pogo', 'mom', 'temporal', 'assassins', 'apocalypse', 'violin',
+    'tentacles', 'transformation', 'ghosts', 'murder', 'gorilla', 'chimpanzee', 'robot', 'monocle', 'superheroes', 'villian', 'commission', 'contract', 'mannequin', 'dolores', 'police', 'officer', 'patch',
+    'hargreeves', 'vietnam', 'love', 'october', 'moon', 'astronaut', 'knife', 'vigilante', 'addict', 'traveler', 'extraordinary', 'siblings', 'family', 'paternity']
 
 //Let's make some variables
 let gameBoard = [];
@@ -12,6 +10,8 @@ let guessBox = [];
 let userGuess;
 let winBox = 0;
 
+//This function handles all of the changes to the HTML that need to happen. Having it up
+//here allows the function to be called from anywhere in the program.
 function displayVarsInPage() {
     document.getElementById("guess-counter").textContent = guessCounter
     document.getElementById("guesses").textContent = guessBox;
@@ -19,18 +19,37 @@ function displayVarsInPage() {
     document.getElementById("game-board").textContent = gameBoardString;
     document.getElementById("win-box").textContent = winBox;
 }
-//Grab random word from word bank for player to guess
+//Grab random word from word bank for player to guess. This function uses Math.random
+//to generate a number between 0-1, uses Math.floor to round that number down to the nearest
+//whole, and then multiplies that number by the length of the wordBank array. That number is 
+//stored in the i variable.The function then returns the word in the array at that random
+//number.
 function generateWord() {
     let i = Math.floor(Math.random() * wordBank.length)
     return wordBank[i];
 };
 
-let word = generateWord();
+//Stored the generateWord function in a global variable so it can be called from anywhere 
+//in the program. 
+let word = "";
 
-//Start game generates a word and then is never start game again
+//Start the game over
+function resetGame() {
+    guessCounter = 0;
+    guessBox = [];
+    gameBoard = [];
+    word = generateWord()
+    for (let i = 0; i < word.length; i++) {
+        gameBoard[i] = "-"
+    }
+};
+
+//Start game generates a word and then is never start game again. In order to have "-"
+//at the start of the game, startGame loops through the word variable, which contains
+//the generateWord function. startGame then shows a "-" at every index of the word (because
+//it loops through it.)
 function startGame() {
     for (let i = 0; i < word.length; i++) {
-        //Dashes at every index of gameBoard
         gameBoard[i] = "-"
 
     }
@@ -43,9 +62,9 @@ function startGame() {
 }
 
 
-// Take user input onkey and turn it into a guess
+// Take user input onkey and turn it into a guess. getGuess is a function that takes one parameter
+//
 function getGuess(event) {
-    //onkey save letter to letter
     let code = event.keyCode
     if (code <= 90 && code >= 65) {
         let letter = event.key
@@ -57,7 +76,7 @@ function getGuess(event) {
     displayVarsInPage();
 };
 
-//Determine if guess matches a letter in the word
+//Determine if guess matches a letter in the word. 
 function isGuessValid(userGuess) {
     let weFoundAMatch = false;
     for (let i = 0; i < word.length; i++) {
@@ -81,26 +100,7 @@ function turnToString(arr) {
     }
     return str;
 }
-// function isGuessValid(userGuess) {
-//     let x = word.indexOf(userGuess)
 
-//     if (x < 0) {
-//         //store userGuess in guessBox
-//         guessBox.push(userGuess)
-//         document.getElementById("guesses").textContent = guessBox
-//         guessCounter++;
-//         document.getElementById("guess-counter").textContent = guessCounter
-//         outOfGuesses();
-
-
-//     } else { //need to modify gameBoard at x
-//         gameBoard[x] = userGuess
-//         console.log("You guessed correctly: " + userGuess)
-//         console.log(gameBoard)
-//         document.getElementById("game-board").textContent = gameBoard
-//         wordComplete();
-//     }
-// }
 //Once every letter in word has been guessed celebrate, then resetGame
 function wordComplete() {
     let gameBoardString = turnToString(gameBoard)
@@ -108,7 +108,7 @@ function wordComplete() {
         document.getElementById("game-board").textContent = word
         celebrate();
         winBox += 1;
-        // resetGame();
+        resetGame();
         let y = document.getElementById("game")
         y.onkeyup = startGame;
 
@@ -127,23 +127,14 @@ function outOfGuesses() {
         // show the word to rub it in their face
         gameBoard = word;
         resetGame();
+        // let y = document.getElementById("game")
+        // y.onkeyup = startGame;
+
     } else {
     }
 }
+//displayVarsInPage();
 
-//Start the game over
-function resetGame() {
-    // then run generate word
-    // clear gameBoard, guessBox, and reset guessCounter
-
-    guessCounter = 0;
-    guessBox = [];
-    word = generateWord()
-    for (let i = 0; i < word.length; i++) {
-        //Dashes at every index of gameBoard
-        gameBoard[i] = "-"
-    }
-};
 
 //User wasn't able to guess the word
 function lose() {
