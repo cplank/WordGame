74d7aa85245acf0224019420f21b953a7590f9b7
cplank <cplankshepard@gmail.com>
2019-03-01 20:11:33 -0800

    final code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-const wordBank = ['love', 'umbrella', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'handler', 'agents', 'time', 'space', 'strength', 'bend', 'metal', 'rumor', 'hypnosis', 'dead', 'commune',
+const wordBank = ['academy', 'umbrella', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'handler', 'agents', 'time', 'space', 'strength', 'bend', 'metal', 'rumor', 'hypnosis', 'dead', 'commune',
     'orphan', 'reginold', 'luther', 'diego', 'allison', 'klaus', 'boy', 'ben', 'vanya', 'chacha', 'hazel', 'agnes', 'doughnuts', 'pogo', 'mom', 'temporal', 'assassins', 'apocalypse', 'violin',
     'tentacles', 'transformation', 'ghosts', 'murder', 'gorilla', 'chimpanzee', 'robot', 'monocle', 'superheroes', 'villian', 'commission', 'contract', 'mannequin', 'dolores', 'police', 'officer', 'patch',
     'hargreeves', 'vietnam', 'love', 'october', 'moon', 'astronaut', 'knife', 'vigilante', 'addict', 'traveler', 'extraordinary', 'siblings', 'family', 'paternity']
@@ -9,6 +9,7 @@ let guessCounter = 0;
 let guessBox = [];
 let userGuess;
 let winBox = 0;
+let messageBox = "";
 
 //This function handles all of the changes to the HTML that need to happen. Having it up
 //here allows the function to be called from anywhere in the program.
@@ -18,6 +19,7 @@ function displayVarsInPage() {
     let gameBoardString = turnToString(gameBoard);
     document.getElementById("game-board").textContent = gameBoardString;
     document.getElementById("win-box").textContent = winBox;
+    document.getElementById("message-box").textContent = messageBox
 }
 //Grab random word from word bank for player to guess. This function uses Math.random
 //to generate a number between 0-1, uses Math.floor to round that number down to the nearest
@@ -33,21 +35,22 @@ function generateWord() {
 //in the program. 
 let word = "";
 
-//Start the game over
+//Reset the game to base (i.e guess counter, clear game board). This function returns the
+//initial state by reseting guessCounter to 0, guessBox to an empty array, gameboard to an empty array,
+//messageBox to an empty string, and then generates a word.
 function resetGame() {
     guessCounter = 0;
     guessBox = [];
     gameBoard = [];
+    messageBox = "";
     word = generateWord()
     for (let i = 0; i < word.length; i++) {
         gameBoard[i] = "-"
     }
 };
 
-//Start game generates a word and then is never start game again. In order to have "-"
-//at the start of the game, startGame loops through the word variable, which contains
-//the generateWord function. startGame then shows a "-" at every index of the word (because
-//it loops through it.)
+//startGame sets the gameBoard to the dashes that represent the length of word, startGame then 
+//runs resetGame which is explained above. I probably don't need this, but I'm scared to get rid of it.
 function startGame() {
     for (let i = 0; i < word.length; i++) {
         gameBoard[i] = "-"
@@ -56,27 +59,34 @@ function startGame() {
     resetGame();
     let y = document.getElementById("game")
     y.onkeyup = getGuess;
-    console.log("Game started")
     displayVarsInPage();
 
 }
 
-
-// Take user input onkey and turn it into a guess. getGuess is a function that takes one parameter
-//
+// Take user input onkey and turn it into a guess. getGuess takes the keycod of the key the 
+//user pressed (I realize now I could have done this using just key instead, and then indexOf to
+//compare -1 or not, but I didn't know that when I made this and it works so I didn't want to 
+//change it). The keycod is then compared, a = 65 and z =90, so if the keycod is between those
+//numbers it's a valid guess. The keycode is stored to the variable letter. If valid, it then runs
+//the isGuessValid function. If it isn't valid, an alert pops up. After the if statement has run,
+//displayVarsInPage runs all the variables that affect the HTML of the page.
 function getGuess(event) {
     let code = event.keyCode
     if (code <= 90 && code >= 65) {
         let letter = event.key
         isGuessValid(letter)
-        console.log(letter);
     } else {
         alert("Please enter a letter a-z")
     }
     displayVarsInPage();
 };
 
-//Determine if guess matches a letter in the word. 
+//Determine if guess matches a letter in the word.isGuessValid takes one parameter, in this case
+//the variable userGuess. If word doesn't match userGuess, weFoundAMatch will be false and the 
+//userGuess will be added to the guessBox, the guessCounter will increase, and the function
+//checks if the player has used their final guess. If userGuess does match, weFoundAMatch will be true. 
+//If true, gameBoard becomes userGuess at the index where the match occurred, and the function
+//runs wordComplete to check if this was the final missing letter in the word.
 function isGuessValid(userGuess) {
     let weFoundAMatch = false;
     for (let i = 0; i < word.length; i++) {
@@ -93,6 +103,11 @@ function isGuessValid(userGuess) {
     }
 }
 
+//Because gameBoard is an array, I had an issue where commas were showing up between each letter.
+//To remedy this, I made this function which takes one parameter, which is represented here by arr (meaning
+//array). It creates a variable, str, which starts as an empty string. The function then loops through
+//the length of arr and adds str to arr at each index. It returns str which will be a string of whatever 
+//array was in the parameter.
 function turnToString(arr) {
     let str = "";
     for (let i = 0; i < arr.length; i++) {
@@ -101,50 +116,55 @@ function turnToString(arr) {
     return str;
 }
 
-//Once every letter in word has been guessed celebrate, then resetGame
+//Once every letter in word has been guessed celebrate. This function creates a variable, gameBoardString
+//which stores the function turnToString called on gameBoard. This will take the gameBoard array and 
+//return a string. If that string equals word, the board will show word and fire celebrate, then increase
+//the number of wins by 1. If gameBoardString doesn't equal word, nothing happens. 
 function wordComplete() {
     let gameBoardString = turnToString(gameBoard)
     if (gameBoardString === word) {
         document.getElementById("game-board").textContent = word
         celebrate();
         winBox += 1;
-        resetGame();
-        let y = document.getElementById("game")
-        y.onkeyup = startGame;
-
-
     } else {
-        // dont do nothin
     }
 }
 
-
-//Determining if the user as used more than 10 guesses
+//Determining if the user as used more than 10 guesses. outOfGuesses checks if the guessCounter
+//has reached 10. If it has, the function fires lose and shows the word for the user. Otherwise,
+//it doess nothing.
 function outOfGuesses() {
-    // if the user ran out of guesses and didn't win, they lose
-    if (guessCounter === 9) {
+    if (guessCounter === 10) {
         lose();
-        // show the word to rub it in their face
         gameBoard = word;
-        resetGame();
-        // let y = document.getElementById("game")
-        // y.onkeyup = startGame;
 
     } else {
     }
 }
-//displayVarsInPage();
 
-
-//User wasn't able to guess the word
+//User wasn't able to guess the word. The lose function alters the message box to let the user
+//know they've used all their guesses. It then waits for a key event to start the game again. Once
+//the key up happens, it runs displayVarsInPage which runs all the variables that affect the HTML
+//of the page.
 function lose() {
-    console.log('you are out of guesses')
+    messageBox = "You've used all of your guesses!"
+    let y = document.getElementById("game")
+    y.onkeyup = startGame;
+    displayVarsInPage();
+
 };
 
-//User guessed the word
+//User guessed the word. celebrate alters the message box to let the user know they've won.
+//It then waits for a key event to start the game again. Once the key up happens, it runs displayVarsInPage 
+//which runs all the variables that affect the HTML of the page.
 function celebrate() {
-    console.log("Yay! You won!")
+    messageBox = "Congrats! You've won!"
+    let y = document.getElementById("game")
+    y.onkeyup = startGame;
+    console.log("Game started")
+    displayVarsInPage();
 };
+
 
 
 
